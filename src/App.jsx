a56519c1cc314ae760447406e1518ad3afabdeec
BragaMPedro <pedrobmagalhaes95@gmail.com>
2022-10-29 08:57:34 -0300

    revisa estrutura global

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { GlobalStyle } from "./Styles/global"
+import { Home } from "./pages/Home/Home"
+
 function App() {
 
   return (
-    <GlobalStyle/>
-    <Home/>    
+    <>
+      <GlobalStyle/>
+      <Home/>    
+    </>
   )
 }
 
