8d1de3b4172388d527f2385aa26a305db84597c6
BragaMPedro <pedrobmagalhaes95@gmail.com>
2022-10-29 08:57:51 -0300

    cria repositório

---
+++ src/utils/listas.js
@@ -0,0 +1,63 @@
+export const listasArray = [
+    {
+        id: Math.random(),
+        nome: "Lista Aniversário",
+        dataCriacao: "10/10/2022",
+        tarefas:[ 
+            {
+                id: Math.random(),
+                nome: "Comprar Bolo",
+            },
+            {
+                id: Math.random(),
+                nome: "Encher Balões",
+            },
+            {
+                id: Math.random(),
+                nome: "Procurar vela do Ben10",
+            },
+            {
+                id: Math.random(),
+                nome: "Enviar Convites",
+            }
+        ]
+    },
+    {
+        id: Math.random(),
+        nome: "Dia de ir no centro",
+        dataCriacao: "26/10/2022",
+        tarefas:[ 
+            {
+                id: Math.random(),
+                nome: "Comprar cebolas",
+            },
+            {
+                id: Math.random(),
+                nome: "Ir oftalmologista",
+            },
+            {
+                id: Math.random(),
+                nome: "Pesquisar preço de Caneta Nankin",
+            }
+        ]
+    },
+    {
+        id: Math.random(),
+        nome: "Médicos",
+        dataCriacao: "15/10/2022",
+        tarefas:[ 
+            {
+                id: Math.random(),
+                nome: "Comprar cebolas",
+            },
+            {
+                id: Math.random(),
+                nome: "Ir oftalmologista",
+            },
+            {
+                id: Math.random(),
+                nome: "Pesquisar preço de Caneta Nankin",
+            }
+        ]
+    }
+]
