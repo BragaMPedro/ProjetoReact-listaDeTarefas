0a4b5f649bfb45b98abeed1b6ad14a8cb7f4be0e
BragaMPedro <pedrobmagalhaes95@gmail.com>
2022-10-31 00:17:02 -0300

    revisa estilo Home

--- src/pages/Home/homeStyle.js
+++ src/pages/Home/homeStyle.js
@@ -10,9 +10,15 @@ export const Content = styled.div`
     flex-direction: column;
     align-items: center;
     width: 77%;
+    height: 100vh;
     padding: 2rem 1rem;
 
     div{
         text-align: center;
+        height: 20%;
+    }
+
+    Lista{
+        height: 80%;
     }
 `;
