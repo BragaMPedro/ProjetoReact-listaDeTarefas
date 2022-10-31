e3014823cecbb2d20fea263e2e22d431459e2f9d
BragaMPedro <pedrobmagalhaes95@gmail.com>
2022-10-31 16:12:20 -0300

    reorganiza Tabela style

--- src/components/Lista/listaTarefasStyle.js
+++ src/components/Lista/listaTarefasStyle.js
@@ -1,44 +1,58 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    
-    table{
-        width: 80%;
-        height: fit-content;
-        max-height: 80%;
-        border-radius: 15px;
-        margin: 2rem 1rem;
-        box-shadow: 0.2rem 0 3rem  #09091961;
-        background-color: aliceblue;
-        
-        thead{
-            border-bottom: 1px solid #090919;
-            margin-bottom: 0.2vh;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   width: 100%;
 
-            tr, th{
-                padding: 0.5rem;
+   table {
+      width: 80%;
+      height: fit-content;
+      max-height: 80%;
+      border-radius: 15px;
+      margin: 2rem 1rem;
+      box-shadow: 0.2rem 0 3rem #09091961;
+      background-color: aliceblue;
+
+      thead {
+         border-bottom: 1px solid #090919;
+         margin-bottom: 0.2vh;
+
+         div{
+            font-size: 1.5rem;
+         }
+
+         th {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+            padding: 0.5rem 1rem;
+
+            .btn-acao {
+               margin: 0 1rem;
+               padding: 0.2rem;
+               height: 2rem;
+               width: 2rem;
+               border-radius: 50%;
             }
-        }
-        
-        tbody{
-            padding: 0.5rem;
-            background-color: aliceblue;
-            filter: brightness(0.8);
 
-            tr{
-              row-gap: 0%;
+            .btn-acao:hover {
+               border: 1px solid #16276c;
+               background-color: #4060e0;
+               color: aliceblue;
+               transition-duration: 0.5s;
+               cursor: pointer;
             }
+         }
+      }
 
-            td{
-                padding: 0.3rem;
-            }
-        }
-    }
-
-    
-`;
+      tbody {
+         padding: 0.5rem;
+         background-color: aliceblue;
+         filter: brightness(0.8);
+      }
+   }
+`
